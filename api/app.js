13556dfba4c15b5aec9db1c7fed06f14f7968ab7
samuelbadru <samuelbadru@outlook.com>
2023-04-26 11:39:44 +0100

    App.js file in api set up

--- api/app.js
+++ api/app.js
@@ -1,2 +1,41 @@
-const express = require('express');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cors = require("cors");
+
+const usersRouter = require("./routes/users");
+
+
 const app = express();
+
+// cors setup
+app.use(cors());
+
+// setup for receiving JSON
+app.use(express.json())
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// route setup
+app.use("/users", usersRouter);
+
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // respond with details of the error
+  res.status(err.status || 500).json({message: 'server error'})
+});
+
+module.exports = app;
