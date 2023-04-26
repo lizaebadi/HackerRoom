aea15969aa6b3f0ecbe3dbac7494698ba41ff146
samuelbadru <samuelbadru@outlook.com>
2023-04-26 14:35:15 +0100

    Token route set up

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const JWT = require("jsonwebtoken");
 const cors = require("cors");
 
 const usersRouter = require("./routes/users");
@@ -19,7 +20,31 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// middleware function to check for valid tokens
+const tokenChecker = (req, res, next) => {
+
+  let token;
+  const authHeader = req.get("Authorization")
+  console.log("authH:", authHeader);
+
+  if(authHeader) {
+    token = authHeader.slice(7)
+  }
+
+  JWT.verify(token, process.env.JWT_SECRET, (err, payload) => {
+    if(err) {
+      console.log(err)
+      res.status(401).json({message: "auth error"});
+    } else {
+      console.log(payload);
+      req.user_id = payload.user_id;
+      next();
+    }
+  });
+};
+
 // route setup
+app.use("/tokens", tokensRouter);
 app.use("/users", usersRouter);
 
 
