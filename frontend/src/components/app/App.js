50bbac955a41bd1a1b410ece8aa924dd5d6688d0
samuelbadru <samuelbadru@outlook.com>
2023-04-27 13:20:48 +0100

    Incorporated game into react app with MainGame component

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -7,11 +7,12 @@ import {
 } from "react-router-dom";
 import SignUpForm from '../signup/SignUpForm';
 import LogInForm from '../login/LogInForm';
+import MainGame from '../game/MainGame';
 
 function App() {
   return (
     <Routes>
-      <Route path='/'  element={<Navigate to="/signup" />}/>
+      <Route path='/'  element={<MainGame />}/>
       <Route path='/signup'  element={<SignUpForm navigate={ useNavigate() } />}/>
       <Route path='/login'  element={<LogInForm  navigate={ useNavigate() }/>}/>
     </Routes>
