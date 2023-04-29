dd52d2a297736868fd1ddf3cddd31221236daf06
samuelbadru <samuelbadru@outlook.com>
2023-04-29 11:10:48 +0100

    Increased expiry time of token to 1 hour

--- api/models/token_generator.js
+++ api/models/token_generator.js
@@ -7,8 +7,8 @@ class TokenGenerator {
       user_id: user_id,
       iat: Math.floor(Date.now() / 1000),
       
-      // Set the JWT token to expire in 10 minutes
-      exp: Math.floor(Date.now() / 1000) + (10 * 60 * 6)
+      // Set the JWT token to expire in 1 hour
+      exp: Math.floor(Date.now() / 1000) + (60 * 60 * 6)
     }, secret);
   }
 }
