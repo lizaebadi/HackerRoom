8318951d97fc7387dddc579cffbf9d48b49320b3
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-04-30 17:54:56 +0100

    experimental collision logic

--- frontend/src/components/game/MainGame.js
+++ frontend/src/components/game/MainGame.js
@@ -13,29 +13,40 @@ const MainGame = ({ }) => {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
       canvas.width =1092;
-      canvas.height = 910;
-    
+      canvas.height = 910;       
+  
       const game = new GameArea(canvas.width, canvas.height);
       const hero = new Hero(game)
       const serverBig = new Object("server-big", 249, 156, 49, 500)
       const wallComputer = new Object("wall-computer", 81, 104, 75, 223)
       const desk = new Object("desk", 235, 117, 270, 270)
-      const table = new Object("table", 116, 139, 910, 700)
       const bookshelf = new Object("bookshelf", 74, 148, 940, 265)
       const wallComputer2 = new Object("wall-computer2", 58, 103, 850, 180)
       const wallServer = new Object("wall-server", 76, 159, 972, 500)
       const floorServer = new Object("floor-server", 52, 99, 49, 730)
+      // x, y, width, height, up, left 
+      const objectList = [
+        {obj: desk, x: 295, y: 240, width: 185, height: 60, down: true},
+        {obj: bookshelf, x: 965, y:220, width: 0, height: 90, left: true},
+        {obj: serverBig, x: 49, y: 520, width: 185, height: 0, down: true},
+        {obj: serverBig, x: 279, y: 550, width: 0, height: 50, right: true},
+        {obj: serverBig, x: 250, y: 550, width: 0, height: 50, left: true},
+        {obj: serverBig, x: 250, y: 510, width: 0, height: 50, right: true},
+        {obj: wallServer, x: 990, y: 560, width: 50, height: 0, left: true},
+        {obj: wallServer, x: 1025, y: 510, width: 0, height: 20, left: true}
+      ]
 
       function animate (){
         ctx.clearRect(0,0, canvas.width, canvas.height);
-        serverBig.render(ctx, hero)
-        wallComputer.render(ctx, hero)
-        wallComputer2.render(ctx, hero)
-        desk.render(ctx, hero)
-        table.render(ctx, hero)
-        bookshelf.render(ctx, hero)
-        wallServer.render(ctx, hero)
-        floorServer.render(ctx, hero)
+        serverBig.render(ctx, hero, objectList)
+        wallComputer.render(ctx, hero, objectList)
+        wallComputer2.render(ctx, hero, objectList)
+        desk.render(ctx, hero, objectList)
+        bookshelf.render(ctx, hero, objectList)
+        wallServer.render(ctx, hero, objectList)
+        floorServer.render(ctx, hero, objectList)
+
+        
         hero.draw(ctx)
         hero.animate()
         requestAnimationFrame(animate);
@@ -49,8 +60,8 @@ const MainGame = ({ }) => {
     <div id="wrapper">
       <canvas ref={canvasRef} id="canvas1"></canvas>
     </div>
-    <img src="$MorganB.png" alt="hero" id="hero"/>
     <img src="server-big.png" alt="server-big" id="server-big"/>
+    <img src="$MorganB.png" alt="hero" id="hero"/>
     <img src="wall-computer.png" alt="wall-computer" id="wall-computer"/>
     <img src="wall-computer2.png" alt="wall-computer2" id="wall-computer2"/>
     <img src="desk.png" alt="desk" id="desk"/>
