b4f05c10485745776704b714ac2f6e3aa3f98f73
Liza Ebadi <111182585+lizaebadi@users.noreply.github.com>
2023-05-03 10:27:53 +0100

    Create CSS for wall computer outro page

--- frontend/src/components/wallComputer/WallComputer.js
+++ frontend/src/components/wallComputer/WallComputer.js
@@ -15,6 +15,8 @@ function WallComputer() {
       } else if (page === "challenge") {
         // it should also check whether the callenge is complete before taking you to outro
         setPage("outro");
+      } else {
+        navigate('/game');
       }
     } else if (event.code === 'Escape') {
       navigate('/game');
@@ -33,9 +35,9 @@ function WallComputer() {
     <div ref={divRef} tabIndex="0">
       {page === "intro" && (
         <div className='wallComputerBackground'>
-        <div id='wallComputerBox'>
-          <h1 id='wallComputer-heading' data-cy="wallComputer-heading">Hello World</h1>
-          <p id='wallComputer-introduction' data-cy="introduction">
+        <div className='wallComputerBox'>
+          <h1 className='wallComputer-heading' data-cy="wallComputer-heading">Hello World</h1>
+          <p id='wallComputer-body' data-cy="introduction">
             To start, we'll be going back to the basics with the classic 'Hello,
             World!' program. But this time, we'll be introducing a whole new
             world - one that the Fakers are determined to exploit. It's up to
@@ -47,7 +49,7 @@ function WallComputer() {
         </div>
       )}
       {page === "challenge" && (
-        <div>
+        <div className='wallComputerBackground'>
           <h1 data-cy="challenge-instructions">
             A simple challenge that requires the user to enter code that outputs
             the Hello World string
@@ -58,15 +60,15 @@ function WallComputer() {
         </div>
       )}
       {page === "outro" && (
-        <div>
-          <h1 data-cy="outro-heading">Congratulations!</h1>
-          <p data-cy="outro-body">
-            (TBC) relevant clue that will lead the user to the next object that
-            they need to find / interact with (TBC) Stored within an object that
-            can be interacted with and shows clues earned{" "}
+        <div className='wallComputerBackground'>
+          <div className='wallComputerBox'>
+          <h1 className='wallComputer-heading' data-cy="outro-heading">Congratulations!</h1>
+          <p id='outro-body' data-cy="outro-body">
+            Head over to the blue desk to explore Variables and Statements
           </p>
-          <p>Hit esc to exit</p>
-          <p>Hit spacebar to continue</p>
+          <p className='exit'>Press Esc to exit</p>
+          <p className='continue'>Press SPACE to continue! ⚔️</p>
+        </div>
         </div>
       )}
     </div>
