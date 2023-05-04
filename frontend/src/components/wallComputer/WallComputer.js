549c5b60b2e626232864140fa6f062d4c30bb81c
Liza Ebadi <111182585+lizaebadi@users.noreply.github.com>
2023-05-04 09:47:55 +0100

    Remove unnecessary import

--- frontend/src/components/wallComputer/WallComputer.js
+++ frontend/src/components/wallComputer/WallComputer.js
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./WallComputer.css";
-import codeImage from './question1.png';
 
 
 function WallComputer() {
