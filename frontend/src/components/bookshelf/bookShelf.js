5fbe9a2a08c2102b53896c21969a9419066e7ce3
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-05-04 12:20:08 +0100

    display loops.md as html

--- frontend/src/components/bookshelf/bookShelf.js
+++ frontend/src/components/bookshelf/bookShelf.js
@@ -12,7 +12,9 @@ export default function Bookshelf() {
         setPage("controlFlow");
       } else if (page === "controlFlow") {
         setPage("lists");
-      }else if (page === "outro") {
+      } else if (page === "lists") {
+        setPage("loops");
+      } else if (page === "outro") {
         navigate("/game");  
       }
     }
@@ -470,6 +472,90 @@ export default function Bookshelf() {
 
         </div>
       )} 
-    </div>
+
+      {page === "loops" && (
+        <div>
+          <h2>break Keyword</h2>
+          <p>In a loop, the break keyword escapes the loop, regardless of the iteration number. Once break executes, the program will continue to execute after the loop.</p>
+          <pre><code class="language-python">numbers = [0, 254, 2, -1, 3]
+
+          for num in numbers:
+            if (num &lt; 0):
+              print(&quot;Negative number detected!&quot;)
+              break
+            print(num)
+            
+          # 0
+          # 254
+          # 2
+          # Negative number detected!
+          </code></pre>
+          <h2>Python List Comprehension</h2>
+          <p>Python list comprehensions provide a concise way for creating lists. It consists of brackets containing an expression followed by a for clause, then zero or more for or if clauses. The expressions can be anything - any kind of object can go into a list. A list comprehension always returns a list.</p>
+          <pre><code class="language-python"># List comprehension for the squares of all even numbers between 0 and 9
+          result = [x**2 for x in range(10) if x % 2 == 0]
+
+          print(result)
+          # [0, 4, 16, 36, 64]
+          </code></pre>
+          <h2>Python For Loop</h2>
+          <p>A Python for loop can be used to iterate over a list of items and perform a set of actions on each item. The syntax of a for loop consists of assigning a temporary value to a variable on each successive iteration.</p>
+          <p>When writing a for loop, remember to properly indent each action, otherwise an IndentationError will result.</p>
+          <pre><code class="language-python"># each num in nums will be printed below
+          nums = [1,2,3,4,5]
+          for num in nums: 
+            print(num)
+          </code></pre>
+          <h2>The Python continue Keyword</h2>
+          <p>In Python, the continue keyword is used inside a loop to skip the remaining code inside the loop code block and begin the next loop iteration.</p>
+          <pre><code class="language-python">big_number_list = [1, 2, -1, 4, -5, 5, 2, -9]
+
+          # Print only positive numbers:
+          for i in big_number_list:
+            if i &lt; 0:
+              continue
+            print(i)
+          </code></pre>
+          <h2>Python Loops with range().</h2>
+          <p>In Python, a for loop can be used to perform an action a specific number of times in a row.</p>
+          <p>The range() function can be used to create a list that can be used to specify the number of iterations in a for loop.</p>
+          <pre><code class="language-python"># Print the numbers 0, 1, 2:
+          for i in range(3):
+            print(i)
+
+          # Print &quot;WARNING&quot; 3 times:
+          for i in range(3):
+            print(&quot;WARNING&quot;)
+          </code></pre>
+          <h2>Infinite Loop</h2>
+          <p>An infinite loop is a loop that never terminates. Infinite loops result when the conditions of the loop prevent it from terminating. This could be due to a typo in the conditional statement within the loop or incorrect logic. To interrupt a Python program that is running forever, press the Ctrl and C keys together on your keyboard.</p>
+          <h2>Python while Loops</h2>
+          <p>In Python, a while loop will repeatedly execute a code block as long as a condition evaluates to True.</p>
+          <p>The condition of a while loop is always checked first before the block of code runs. If the condition is not met initially, then the code block will never run.</p>
+          <pre><code class="language-python"># This loop will only run 1 time
+          hungry = True
+          while hungry:
+            print(&quot;Time to eat!&quot;)
+            hungry = False
+
+          # This loop will run 5 times
+          i = 1
+          while i &lt; 6:
+            print(i)
+            i = i + 1
+          </code></pre>
+          <h2>Python Nested Loops</h2>
+          <p>In Python, loops can be nested inside other loops. Nested loops can be used to access items of lists which are inside other lists. The item selected from the outer loop can be used as the list for the inner loop to iterate over.</p>
+          <pre><code class="language-python">groups = [[&quot;Jobs&quot;, &quot;Gates&quot;], [&quot;Newton&quot;, &quot;Euclid&quot;], [&quot;Einstein&quot;, &quot;Feynman&quot;]]
+
+          # This outer loop will iterate over each list in the groups list
+          for group in groups:
+            # This inner loop will go through each name in each list
+            for name in group:
+              print(name)
+          </code></pre>
+        </div>
+      )} 
+      </div>
   )};
       
