dfad6ec2cf180d6f07744e8d0efcb9bd8ccdf16b
Liza Ebadi <111182585+lizaebadi@users.noreply.github.com>
2023-05-04 09:28:45 +0100

    Add function and return for multiple choice logic

--- frontend/src/components/wallComputer/WallComputer.js
+++ frontend/src/components/wallComputer/WallComputer.js
@@ -1,26 +1,44 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./WallComputer.css";
-import CodeEditor from '../codeMirror/codeMirror.js';
+
 
 function WallComputer() {
   const [page, setPage] = useState("intro");
   const divRef = useRef(null);
   const navigate = useNavigate();
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [message, setMessage] = useState('');
+
+  function submitAnswer() {
+    if (selectedAnswer === questions[currentQuestion].correctAnswer) {
+      // answer is correct
+      setMessage("Correct!");
+      if (currentQuestion === questions.length - 1) {
+        setPage("outro");
+      } else {
+        // takes you to the next question
+        setCurrentQuestion(currentQuestion + 1);
+        setSelectedAnswer(null);
+        setTimeout(() => setMessage(''), 3000); // hide message after 3 seconds
+      }
+    } else {
+      // answer is incorrect
+      setMessage("Not quite, try again!");
+      setTimeout(() => setMessage(''), 3000); // hide message after 3 seconds
+    }
+  }
 
   function handleKeyDown(event) {
-
     if (event.code === "Space") {
       if (page === "intro") {
         setPage("challenge");
-      } else if (page === "challenge") {
-        // it should also check whether the callenge is complete before taking you to outro
-        setPage("outro");
-      } else {
-        navigate('/game');
+      } else if (page === "outro") {
+        navigate("/game");  
       }
-    } else if (event.code === 'Escape') {
-      navigate('/game');
+    } else if (event.code === "Escape") {
+      navigate("/game");
     }
   }
 
@@ -31,6 +49,15 @@ function WallComputer() {
     window.removeEventListener('keydown', handleKeyDown);
   };
   });
+
+  const questions = [
+    {
+      question: "What is the syntax for declaring a variable in Python?",
+      codeImage: null,
+      answers: ["var = 10", "variable 10", "int var = 10", "var := 10"],
+      correctAnswer: 0,
+    },
+  ]
 
   return (
     <div ref={divRef} tabIndex="0">
@@ -50,15 +77,28 @@ function WallComputer() {
         </div>
       )}
       {page === "challenge" && (
-
-        <div className='wallComputerBackground'> 
-          <h1 data-cy="challenge-instructions">
-            A simple challenge that requires the user to enter code that outputs
-            the Hello World string
-          </h1>
-          
-          
-          
+        <div className="wallComputerBackground">
+          <div id="question-box" key={currentQuestion}>
+            <h2 id="current-question">{questions[currentQuestion].question}</h2>
+            {questions[currentQuestion].answers.map((answer, answerIndex) => (
+              <div className="answer" key={answerIndex}>
+                <div
+                  className="container"
+                  onClick={() => setSelectedAnswer(answerIndex)}
+                >
+                  <div className="checkbox">
+                    {selectedAnswer === answerIndex && (
+                      <div className="checked"></div>
+                    )}
+                  </div>
+                  <div className="label">{answer}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button onClick={submitAnswer}>Submit</button>
+          <p className="exit">Press Esc to exit</p>
+          {message && <p className="message">{message}</p>}
         </div>
       )}
       {page === "outro" && (
