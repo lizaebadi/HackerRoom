fc459a13915f8a365add9f5601f5ae042284ba5a
samuelbadru <samuelbadru@outlook.com>
2023-05-04 12:02:06 +0100

    First draft of intro message

--- frontend/src/components/intro/Intro.js
+++ frontend/src/components/intro/Intro.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './Intro.css'
 
 const Intro = () => {
+  const userData = JSON.parse(window.localStorage.getItem("user"));
+  const username = userData.username;
 
   return (
-    <>
-    </>
+    <div id='intro-container'>
+      <h1>Welcome ${username}!</h1>
+      <p>Get ready to hone your coding skills and knowledge while having fun.</p>
+      <p>Use the Arrow keys to move your character around the room</p>
+      <p>Use the Space bar to interact with objects</p>
+      <p>Use the Esc key to access the menu </p>
+      <p>Enjoy HackerRoom</p>
+    </div>
   )
 
 }
